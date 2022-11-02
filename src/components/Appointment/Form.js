f04cce20ae481de7fd31d50ebcbdee3888f04c25
Jillian Aubrey <jillian.nk.aubrey@gmail.com>
2022-11-02 16:57:31 -0600

    Refactor Appointment Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,18 +5,31 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || '');
   const [interviewerId, setInterviewerId] = useState(props.interviewer || null)
+  
+  const reset = () => {
+    setStudent(props.student || '');
+    setInterviewerId(props.interviewer || null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form 
+         autoComplete="off"
+         onSubmit={event => event.preventDefault()}
+        >
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={student}
-            onChange={() => setStudent(student)}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
@@ -29,13 +42,13 @@ export default function Form(props) {
         <section className="appointment__actions">
           <Button 
             danger
-            onClick={props.onCancel}
+            onClick={cancel}
           >
             Cancel
           </Button>
           <Button 
             confirm
-            onClick={props.onSave}
+            onClick={() => props.onSave(student, interviewerId)}
           >
             Save
           </Button>
